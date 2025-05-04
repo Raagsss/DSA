90e692838f3cde13ab2f967e9df229a0e36bf466
Raghav Rana <139216288+Raagsss@users.noreply.github.com>
2025-05-04 10:45:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (49.62%) - LeetSync

---
+++ LeetCode/551-student-attendance-record-i/student-attendance-record-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int absents=0;
+        for(int i=0;i<s.length();i++) {
+            if(s[i]=='A') {
+                absents++;
+                if(absents>=2) {
+                    return false;
+                }
+            }
+            if(i>=2 && s[i]=='L' && s[i-1]=='L' && s[i-2]=='L') {
+                return false;
+            }
+        }
+        return true;
+    }
+};
