505321536dd6e8b5e905b331d7169cf8c11d5e2f
Raghav Rana <139216288+Raagsss@users.noreply.github.com>
2025-07-08 14:32:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (5.56%) - LeetSync

--- LeetCode/58-length-of-last-word/length-of-last-word.cpp
+++ LeetCode/58-length-of-last-word/length-of-last-word.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int length=0;
-        int i=s.length()-1;
-        while (i>=0 && s[i]==' ') 
-            i--;
+        // int length=0;
+        // int i=s.length()-1;
+        // while (i>=0 && s[i]==' ') 
+        //     i--;
 
-        while (i>=0 && s[i]!=' ') {
-            length++;
-            i--;
+        // while (i>=0 && s[i]!=' ') {
+        //     length++;
+        //     i--;
+        // }
+        // return length;
+
+        stringstream ss(s);
+        string word,lastword;
+        while(ss >> word) {
+            lastword=word;
         }
-        return length;
+        return lastword.length();
     }
 };
