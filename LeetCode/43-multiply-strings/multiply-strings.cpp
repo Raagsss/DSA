0aad6d0e854823e1247a6536725069a54f6d4249
Raghav Rana <139216288+Raagsss@users.noreply.github.com>
2025-09-14 11:05:58 +0530

    Time: 4 ms (35.71%) | Memory: 9.6 MB (71.89%) - LeetSync

--- LeetCode/43-multiply-strings/multiply-strings.cpp
+++ LeetCode/43-multiply-strings/multiply-strings.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        if (num1=="0"||num2=="0") 
-            return "0";
+        if (num1 == "0" || num2 == "0") return "0";
 
-        int n=num1.size();
-        int m=num2.size();
-        vector<int> result(n+m,0);
+        int n = num1.size(), m = num2.size();
+        vector<int> res(n + m, 0);
 
-        for(int i=n-1;i>=0;i--) {
-            for(int j=m-1;j>=0;j--) {
-                int mul=(num1[i]-'0')*(num2[j]-'0');
-                int sum=mul+result[i+j+1];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int mul = (num1[i] - '0') * (num2[j] - '0');
+                int sum = mul + res[i + j + 1];
 
-                result[i+j+1]=sum%10;
-                result[i+j]+=sum/10;
+                res[i + j + 1] = sum % 10;
+                res[i + j] += sum / 10;
             }
         }
 
-        string res;
-        for(int num:result) {
-            if(!(res.empty() && num==0)) {
-                res+=(num+'0');
-            }
+        string ans = "";
+        for (int x : res) {
+            if (!(ans.empty() && x == 0))
+                ans.push_back(x + '0');
         }
-        return res;
+
+        return ans;
     }
 };
