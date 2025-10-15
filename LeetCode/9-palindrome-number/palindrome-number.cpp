d3e0e4810fcf3b11a8f453584d9c322ab2f8a581
Raghav Rana <139216288+Raagsss@users.noreply.github.com>
2025-10-15 10:50:40 +0530

    Time: 4 ms (32.61%) | Memory: 8.5 MB (91.96%) - LeetSync

--- LeetCode/9-palindrome-number/palindrome-number.cpp
+++ LeetCode/9-palindrome-number/palindrome-number.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // long long ans=0;
-        if(x<0) {
-            return false;
-        }    
-        // int temp=x;
-        // while(temp>0) {
-        //     ans=ans*10+(temp%10);
-        //     temp=temp/10;
-        // }
+        if(x<0 || x%10==0 & x!=0) return false;
+        int p=0;
 
-        // return ans==x;
-
-        string p=to_string(x);
-        string rev="";
-        // reverse(rev.begin(),rev.end());
-        for(int i=p.length()-1;i>=0;i--) {
-            rev+=p[i];
+        while(x>p) {
+            p=(p*10)+(x%10);
+            x=x/10;
         }
 
-        return rev==p;
-        
+        return p==x || x==p/10;
     }
 };
