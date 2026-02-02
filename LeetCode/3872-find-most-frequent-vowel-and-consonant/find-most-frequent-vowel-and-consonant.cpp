9c13eabec6cbe0fd015bb0299328b1123df06a98
Raghav Rana <139216288+Raagsss@users.noreply.github.com>
2026-02-02 18:00:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (13.88%) - LeetSync

--- LeetCode/3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ LeetCode/3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
-        unordered_map<char,int> vw;
-        unordered_map<char,int> cn;
-        string vowels = "aeiouAEIOU";
+        unordered_map<char,int> vowels;
+        unordered_map<char,int> consonant;
 
-        for(char it:s) {
-            if(isalpha(it)) {
-                if(vowels.find(it)!=string::npos) {
-                    vw[it]++;
-                }else {
-                    cn[it]++;
-                }
+        for(auto it:s) {
+            if(it=='a' || it=='e' || it=='i' || it=='o' || it=='u') {
+                vowels[it]++;
+            }else {
+                consonant[it]++;
+            }
+        }
+        
+        int maxVowel=0;
+        for(auto it:vowels) {
+            if(it.second>maxVowel) {
+                maxVowel=it.second;
             }
         }
 
-        int maxVowel=0;
-        int maxConsonant=0;
-
-        for(auto it:vw) {
-            maxVowel=max(maxVowel,it.second);
+        int maxConst=0;
+        for(auto it:consonant) {
+            if(it.second>maxConst) {
+                maxConst=it.second;
+            }
         }
 
-        for(auto it:cn) {
-            maxConsonant=max(maxConsonant,it.second);
-        }
-
-        return maxVowel+maxConsonant;
+        return maxConst+maxVowel;
     }
 };
